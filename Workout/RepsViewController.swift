cc56c34e0fcf83d75140d743a4ee671131ae2f91
Warren Hansen <whansen1@mac.com>
2018-03-07 14:56:00 -0800

    task: mark sets done with color

--- Workout/RepsViewController.swift
+++ Workout/RepsViewController.swift
@@ -12,31 +12,33 @@ import RealmSwift
 class RepsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableview: UITableView!
-    
     @IBOutlet weak var weightLable: UILabel!
+    @IBOutlet weak var workOutComplereBttn: UIButton!
     
-    var repsArray:[String] = []
+    
+    //var repsArray:[String] = []
+    var repsIntArray:[Int] = []
     var exerciseTitle:String = ""
     var taskID = ""
     var task:Exercises?
+    var tasksCompleted:[Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         task = Exercises().getExerciseBy(taskID: taskID, debug: true)
         populateTableview()
-        // mark sets done
-        // load weight
+        // mark exercise donr in database
         // change weight
     }
     
-   
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repsArray.count
+        return repsIntArray.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = repsArray[indexPath.row]
+        cell.textLabel?.text = "Set \(repsIntArray[indexPath.row] + 1)"
+        if tasksCompleted.contains(indexPath.row) { cell.contentView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1) }
         return cell
     }
 
@@ -48,14 +50,35 @@ class RepsViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return  exerciseTitle
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        checkForSetsCompleted(row: indexPath.row, debug: true)
+    }
+    
+    func checkForSetsCompleted(row:Int, debug:Bool) {
+        print("tapped row \(row)")
+        tasksCompleted.append(row)
+        if debug { debugPrint(tasksCompleted) }
+        let workoutComplete = tasksCompleted.contains(repsIntArray)
+        if debug { print("it's \(workoutComplete) that the workout is complete") }
+        tableview.reloadData()
+        
+        if workoutComplete {
+            print("Sets complete")
+            workOutComplereBttn.setTitle("Workout Complete",for: .normal)
+            workOutComplereBttn.backgroundColor = #colorLiteral(red: 0.3615907431, green: 0.6538704038, blue: 0.172421515, alpha: 1)
+        }
+    }
+    
     func populateTableview() {
         
         if let reps = task?.sets {
-            for i in 1 ... reps {
-                repsArray.append("Set \(i)")
+            for i in 0 ..< reps {
+                repsIntArray.append(i)
             }
         }
         if let exercise = task?.type {  exerciseTitle = exercise }
         if let weightFound = task?.weight { weightLable.text = "\(weightFound) lbs" }
     }
 }
+
+
