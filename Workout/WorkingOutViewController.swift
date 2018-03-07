5a247f5c2b6ef38bcf3d0698e35bcf832e680c20
Warren Hansen <whansen1@mac.com>
2018-03-06 18:20:32 -0800

    task: detail now loaded in tableview

--- Workout/WorkingOutViewController.swift
+++ Workout/WorkingOutViewController.swift
@@ -7,50 +7,40 @@
 //
 
 import UIKit
+import RealmSwift
 
 class WorkingOutViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableview: UITableView!
     var lastWorkoutText:String = ""
     var startBttnTxt:String = ""
+    var tasks = Exercises().getNextWorkout()
     
-    var titleArray:[String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let results = Exercises().getNextWorkout()
-        
-        if let char = results.last?.group {
-            title = "Workout \(char)"
+        if let group = tasks.last?.group {
+            title = "Workout \(group)"
         }
-        
-        let typeArray: [String] = results.map { (type: Exercises) in
-            return type.type
-        }
-        
-        for each in typeArray {
-            titleArray.append(each)
-        }
-        
-        // todo sets reps weigh in 2nd row
-        // todo clicking on row changes color and sets a counter
-        // if we segue out with counter at 5, give the goup a date
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titleArray.count
+        return tasks.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
         // for custom cell
         //let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as!StockTableViewCell
-        cell.textLabel?.text = titleArray[indexPath.row]
-        
+        cell.textLabel?.text = tasks[indexPath.row].type
+        cell.detailTextLabel?.text = setDetailForTableview(row: indexPath.row)
         return cell
     }
-    
+
+    func setDetailForTableview(row:Int) -> String {
+        return"\(tasks[row].sets) Sets \t \(tasks[row].reps) reps\t \(tasks[row].weight) lbs"
+       
+    }
     
 
 }
